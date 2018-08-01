3d59a183e45c8c67ec8544212ff24248a0ea9695
David K. Bainbridge <dbainbri@ciena.com>
2018-08-01 11:03:50 -0700

    handle scapy packet processing better

--- dhcp-relay.py
+++ dhcp-relay.py
@@ -25,45 +25,43 @@ def num(d):
     return v
 
 # used to store the dpid and port associated with a DHCP (BOOTP) transaction ID
+# Not real great practice for x-thread comms, but this is a hack
 xids = {}
 
-# Thread used to sniff for DHCP responses and packet out them back to the
-# client
-def responder():
-    while True:
-        ps=scapy.sniff(filter="udp and dst port 68", count=5)
-        # Iterate over the packets we recieved looking for BOOTP packets
-        for p in ps:
-            # Make sure we have a packet we really want to send to the client.
-            # Not sure why the sniff filter isn't work, hmm ...
-            if scapy.BOOTP in p and p[scapy.UDP].dport == 68:
+# Process a DHCP reponse packet  and packet out a response
+def process(p):
+    if scapy.BOOTP in p and p[scapy.UDP].dport == 68:
 
-                # Lookup DPID and port based on transaction ID
-                if p[scapy.BOOTP].xid not in xids:
-                    print "UNEXPECTED/UNKNOWN XID: %s" % p[scapy.BOOTP].xid
-                    sys.stdout.flush()
-                    continue
+        # Lookup DPID and port based on transaction ID
+        if int(p[scapy.BOOTP].xid) not in xids:
+            print "UNEXPECTED/UNKNOWN XID: '%s'" % int(p[scapy.BOOTP].xid)
+            sys.stdout.flush()
+            return
 
-                dpid,port,dst=xids[p[scapy.BOOTP].xid]
-                print "XID: %s [RESPONSE]" % p[scapy.BOOTP].xid
-                print "    DPID: 0x%s" % hex(dpid)
-                print "    PORT: %d" % port
-                print "    DST:  %s" % dst
-                print "    IP:   %s" % p[scapy.BOOTP].yiaddr
-                sys.stdout.flush()
+        dpid,port,dst=xids[p[scapy.BOOTP].xid]
+        print "XID: %s [RESPONSE]" % p[scapy.BOOTP].xid
+        print "    DPID: 0x%s" % hex(dpid)
+        print "    PORT: %d" % port
+        print "    DST:  %s" % dst
+        print "    IP:   %s" % p[scapy.BOOTP].yiaddr
+        sys.stdout.flush()
 
-                # Set the DST to the client
-                p[scapy.Ether].dst = dst
+        # Set the DST to the client
+        p[scapy.Ether].dst = dst
 
-                # Create an OF packet out and send it back through OFTEE
-                po = of.OFPTPacketOut(data=p)
-                po.in_port = 0xffffffff # ANY PORT
-                po.actions = [of.OFPATOutput(port=port, pad=[0,0,0,0])]
-                po.len = len(bytes(po))
-                po.actions_len = 16
-                res = requests.post(url='http://oftee:8000/oftee/0x%s' % hex(dpid),
-                    data=bytes(po),
-                    headers={'Content-Type': 'application/octet-stream'})
+        # Create an OF packet out and send it back through OFTEE
+        po = of.OFPTPacketOut(data=p)
+        po.in_port = 0xffffffff # ANY PORT
+        po.actions = [of.OFPATOutput(port=port, pad=[0,0,0,0])]
+        po.len = len(bytes(po))
+        po.actions_len = 16
+        res = requests.post(url='http://oftee:8000/oftee/0x%s' % hex(dpid),
+            data=bytes(po),
+            headers={'Content-Type': 'application/octet-stream'})
+
+# Thread used to sniff for DHCP responses and send them to a processor
+def sniffer():
+    scapy.sniff(filter="udp and dst port 68", prn=process)
 
 @app.route('/packet', methods=['POST'])
 def packet_in():
@@ -79,6 +77,8 @@ def packet_in():
     print "    PORT: %d" % num(data[8:12])
     print "    DST:  %s" % packet[scapy.Ether].src
     sys.stdout.flush()
+
+    # store the XIX mapping to the worker (response) thread
     xids[packet[scapy.BOOTP].xid] = (
             data[0:8],
             num(data[8:12]),
@@ -106,10 +106,10 @@ def packet_in():
     r = scapy.send(out[scapy.IP])
     return "", 201
 
-# Start the capture thread
-capture = t.Thread(target=responder)
-capture.daemon = True
-capture.start()
+if __name__ == "__main__":
+    capture = t.Thread(target=sniffer)
+    capture.daemon = True
+    capture.start()
 
-# Start the proxy app, listening for packet in requests from OFTEE
-app.run(host='0.0.0.0', debug=True)
+    # Start the proxy app, listening for packet in requests from OFTEE
+    app.run(host='0.0.0.0', debug=True, use_reloader=False)
